e5765408757974a22e8baec026e181c554062552
Ben Bullock <benkasminbullock@gmail.com>
2021-02-18 22:49:15 +0900

    Tone down colours some more

--- sane-perl-mode.el
+++ sane-perl-mode.el
@@ -572,7 +572,7 @@ This way enabling/disabling of menu items is more correct."
     (((class grayscale) (background dark))
      (:foreground "Gray80" :weight bold))
     (((class color) (background light))
-     (:foreground "Blue" :background "lightyellow2" :weight bold))
+     (:foreground "Blue"))
     (((class color) (background dark))
      (:foreground "yellow" :background ,sane-perl-dark-background :weight bold))
     (t (:weight bold)))
